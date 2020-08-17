75ac75c8dd7e2af81f3ed3db786341a23b2a0f7f
Umer Nasir <umernasirr@gmail.com>
2020-08-17 16:48:12 +0500

    Finished Todo App

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-
+import { MaterialIcons } from "@expo/vector-icons";
 export default function TodoItem({ item, todoHandler }) {
   return (
     <TouchableOpacity
@@ -8,18 +8,25 @@ export default function TodoItem({ item, todoHandler }) {
         todoHandler(item);
       }}
     >
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <MaterialIcons name="delete" size={20} color="#333" />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   item: {
+    flexDirection: "row",
     padding: 16,
     marginTop: 16,
     borderColor: "#bbb",
     borderWidth: 1,
     borderStyle: "dashed",
     borderRadius: 10,
+  },
+  itemText: {
+    marginLeft: 10,
   },
 });
